c40540a1bfb8bb413a6a3828c7c36165b38d5108
Stanislav Sudakov <stasiksudakov@gmail.com>
2024-11-15 21:26:39 +0200

    add weather api

--- weather_checker.py
+++ weather_checker.py
@@ -1,9 +1,8 @@
-from datetime import datetime
 import os
-from dotenv import load_dotenv
 
-import httpx
+import requests
 import json
+from dotenv import load_dotenv
 
 load_dotenv()
 
@@ -11,29 +10,26 @@ API_KEY = os.environ.get("WEATHER_API_KEY")
 BASE_URL = os.environ.get("WEATHER_BASE_URL")
 
 
-def get_weather(client: httpx.Client, city) -> dict | None:
+def get_weather(city) -> dict | None:
     try:
-        response = client.get(BASE_URL,
-                    params={"key": API_KEY, "q": city})
-        response.raise_for_status()
-        data = response.json()
+        res = requests.get(BASE_URL, params={"key": API_KEY, "q": city})
+        res.raise_for_status()
+        data = res.json()
 
-        temperature = {
-            "city": city,
-            "temperature": data["current"]["temp_c"],
-            "date_time": datetime.now()
-        }
-        return temperature
+        location_name = data["location"]["name"]
+        location_country = data["location"]["country"]
+        localtime = data["location"]["localtime"]
+        temp_c = data["current"]["temp_c"]
+        condition_text = data["current"]["condition"]["text"]
 
-    except httpx.RequestError as e:
+        return f"{location_name}/{location_country}\n{localtime}\nWeather: {temp_c} Celsius, {condition_text}"
+
+
+    except requests.RequestException as e:
         print(f"Network or HTTP error occurred: {e}")
-        return None
     except json.JSONDecodeError:
         print("Error decoding JSON response.")
-        return None
     except KeyError as e:
         print(f"Missing expected key in the response: {e}")
-        return None
     except Exception as e:
         print(f"An unexpected error occurred: {e}")
-        return None
