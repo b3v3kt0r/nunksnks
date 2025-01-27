888abfbd788e1563d9a205836f23d110a37c03ac
Stanislav Sudakov <stasiksudakov@gmail.com>
2025-01-27 20:08:13 +0200

    add parser for dou

--- main.py
+++ main.py
@@ -10,7 +10,11 @@ from pymongo.server_api import ServerApi
 import ai_helper
 from helpers import is_cyrillic_only
 from keep_alive import run_flask
-from parser import parse_news_playua, parse_steam_sale_date
+from parser import (
+    parse_news_playua, 
+    parse_steam_sale_date,
+    parse_dou_for_activities
+)
 from weather_checker import get_weather
 
 load_dotenv()
@@ -24,17 +28,22 @@ db = client["bot_data"]
 
 @bot.message_handler(commands=["start"])
 def start_message(message):
+    """
+    Base function with buttons.
+    """
     nickname = message.from_user.first_name or message.username or message.from_user.last_name
 
     markup = telebot.types.ReplyKeyboardMarkup()
-    get_weather = telebot.types.KeyboardButton("Get the weather")
-    get_articles = telebot.types.KeyboardButton("Get PlayUA top articles")
-    get_steam_sale = telebot.types.KeyboardButton("Steam sale")
-    markup.row(get_weather, get_articles, get_steam_sale)
-    leave_note = telebot.types.KeyboardButton("Leave a note")
-    get_notes = telebot.types.KeyboardButton("My notes")
+    get_weather = telebot.types.KeyboardButton("Get the weather🌤")
+    get_articles = telebot.types.KeyboardButton("Get PlayUA top articles🗞")
+    get_steam_sale = telebot.types.KeyboardButton("Steam sale🔥")
+    get_dou_activities = telebot.types.KeyboardButton("DOU activities📅")
+    markup.row(get_articles, get_dou_activities)
+    markup.row(get_weather, get_steam_sale)
+    leave_note = telebot.types.KeyboardButton("Leave a note📝")
+    get_notes = telebot.types.KeyboardButton("My notes📑")
     markup.row(leave_note, get_notes)
-    markup.add(telebot.types.KeyboardButton("Who's creator of this godness?"))
+    markup.add(telebot.types.KeyboardButton("Who's creator of this goodness?"))
     markup.add(telebot.types.KeyboardButton("Leave the feedback"))
 
     bot.reply_to(message, f"Hello, {nickname}! My name is Nunksnks.", reply_markup=markup)
@@ -42,20 +51,29 @@ def start_message(message):
 
 @bot.message_handler(func=lambda message: message.text == "Who's creator of this godness?")
 def answer_about_creator(message):
-    bot.reply_to(message, "My creator's name is The Great Stanislav")
+    """
+    Button that return name of creator.
+    """
+    bot.reply_to(message, "My creator's name is The Great Stanislav🤴")
 
 
 @bot.message_handler(func=lambda message: message.text == "Leave the feedback")
 def feedback(message):
+    """
+    Button that triggers leaving feedback process.
+    """
     markup = telebot.types.InlineKeyboardMarkup()
-    bt1 = telebot.types.InlineKeyboardButton("I like it!", callback_data="Like")
-    bt2 = telebot.types.InlineKeyboardButton("I'm bad person!", callback_data="Dislike")
+    bt1 = telebot.types.InlineKeyboardButton("I like it!👍", callback_data="Like")
+    bt2 = telebot.types.InlineKeyboardButton("I'm bad person!👎", callback_data="Dislike")
     markup.add(bt1, bt2)
     bot.reply_to(message, "You can leave only positive feedback or we will find you!", reply_markup=markup)
 
 
 @bot.callback_query_handler(func=lambda call: True)
 def handle_feedback(call):
+    """
+    Function for handling feedback.
+    """
     nickname = call.from_user.first_name or call.from_user.username or call.from_user.last_name
     feedback = call.data
     user_id = call.from_user.id
@@ -78,18 +96,24 @@ def handle_feedback(call):
 
     if feedback == "Like":
         # bot.answer_callback_query(call.id, "Bless you!")
-        bot.send_message(call.message.chat.id, "Bless you!")
+        bot.send_message(call.message.chat.id, "Bless you!🫶")
     elif feedback == "Dislike":
         # bot.answer_callback_query(call.id, "We will find you!")
-        bot.send_message(call.message.chat.id, "We will find you!")
+        bot.send_message(call.message.chat.id, "We will find you!👊")
 
 
-@bot.message_handler(func=lambda message: message.text == "Get the weather")
+@bot.message_handler(func=lambda message: message.text == "Get the weather🌤")
 def weather(message):
-        bot.reply_to(message, "Write name of the city")
-        bot.register_next_step_handler(message, city_name)
+    """
+    Button that triggers getting weather process.
+    """
+    bot.reply_to(message, "Write name of the city")
+    bot.register_next_step_handler(message, city_name)
 
 def city_name(message):
+    """
+    Function that check weather of given city.
+    """
     city = message.text
     weather = get_weather(city)
     bot.reply_to(message, weather)
@@ -98,12 +122,18 @@ def city_name(message):
 # regexp=r"^(Who\'s creator of this godness\?|Leave the feedback|Get the weather|Leave a note)$"
 
 
-@bot.message_handler(func=lambda message: message.text == "Leave a note")
+@bot.message_handler(func=lambda message: message.text == "Leave a note📝")
 def note(message):
+    """
+    Button that triggers saving of note process.
+    """
     bot.reply_to(message, "Please, write it in the chat")
     bot.register_next_step_handler(message, save_note)
 
 def save_note(message):
+    """
+    Function that save note to db.
+    """
     nickname = message.from_user.first_name or message.username or message.from_user.last_name
     user_id = message.from_user.id
 
@@ -119,26 +149,35 @@ def save_note(message):
     bot.reply_to(message, "Nice one! You can check all your notes by writing \"My notes\" in the chat")
 
 
-@bot.message_handler(func=lambda message: message.text == "Get PlayUA top articles")
+@bot.message_handler(func=lambda message: message.text == "Get PlayUA top articles🗞")
 def get_playua_articles(message):
     articles = parse_news_playua()
-    articles_to_send = ""
-    counter = 0
-    for article in articles:
-        counter += 1
-        articles_to_send += f"{counter}. {article}\n"
-    articles_to_send += "<a href='https://playua.net/'><b>PlayUA website</b></a>"
-    bot.reply_to(message, articles_to_send, parse_mode="HTML")
+    bot.reply_to(message, articles, parse_mode="HTML")
 
 
-@bot.message_handler(func=lambda message: message.text == "Steam sale")
+@bot.message_handler(func=lambda message: message.text == "DOU activities📅")
+def get_dou_activities_lviv(message):
+    """
+    Button that parse dou activities in Lviv and return it.
+    """
+    activities = parse_dou_for_activities()
+    bot.reply_to(message, activities, parse_mode="html")
+
+
+@bot.message_handler(func=lambda message: message.text == "Steam sale🔥")
 def find_out_steam_sale(message):
+    """
+    Button that check if there is steam sale right now.
+    """
     sale = parse_steam_sale_date()
     bot.reply_to(message, sale)
 
 
-@bot.message_handler(func=lambda message: message.text == "My notes")
+@bot.message_handler(func=lambda message: message.text == "My notes📑")
 def get_all_notes(message):
+    """
+    Button that gives to us the all notes that we made before.
+    """
     user_id = message.from_user.id
 
     collection = db["notes"]
@@ -150,7 +189,9 @@ def get_all_notes(message):
 
 @bot.message_handler(content_types=["text"])
 def info(message):
-
+    """
+    Function that responses for all text except buttons. Check text for toxicity, save it to db and give response from AI.
+    """
     nickname = message.from_user.first_name or message.username or message.from_user.last_name
     user_id = message.from_user.id
 
@@ -170,8 +211,8 @@ def info(message):
 
     toxicity = ai_helper.check_for_toxicity(message_text)
     if toxicity:
-        bot.send_message(message.chat.id, "Fuck you!")
-        return 
+        bot.send_message(message.chat.id, "Fuck you!🖕🖕🖕")
+        return
 
     response = ai_helper.auto_replay_for_message(message_text)
     bot.send_message(message.chat.id, response)
@@ -184,5 +225,5 @@ def start_bot():
     bot.infinity_polling()
 
 if __name__ == "__main__":
-    start_flask_thread()
+    # start_flask_thread()
     start_bot()
--- parser.py
+++ parser.py
@@ -1,17 +1,22 @@
 from datetime import datetime
-import random
+import html
 import requests
+
 from bs4 import BeautifulSoup
 
 
 PLAY_UA = "https://playua.net/"
 STEAM_SALES = "https://steambase.io/sales"
+DOU_LVIV_ACTIVITIES = "https://dou.ua/calendar/city/Lviv/"
 
 
 def fetch_html(url):
     text = requests.get(
-        url, 
-        headers={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36"}).content
+        url,
+        headers={
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36"
+        },
+    ).content
     soup = BeautifulSoup(text, "html.parser")
     return soup
 
@@ -22,9 +27,15 @@ def parse_one_article_playua(article):
 
 def parse_news_playua():
     soup = fetch_html(PLAY_UA)
-
     articles = soup.select(".short-article")
-    return [parse_one_article_playua(article) for article in articles]
+    articles_list = [parse_one_article_playua(article) for article in articles]
+    articles_to_send = ""
+    counter = 0
+    for article in articles_list:
+        counter += 1
+        articles_to_send += f"<b>{counter}.</b> {article}.\n\n"
+    articles_to_send += "<a href='https://playua.net/'><b>PlayUA website</b></a>"
+    return articles_to_send
 
 
 def parse_steam_sale_date():
@@ -43,3 +54,33 @@ def parse_steam_sale_date():
             return f"{name}\nDate of end: {norm_date}"
     except AttributeError:
         return "There are no information about steam sales right now!"
+
+
+def parse_one_dou_activity(activity):
+    def clean_text(text):
+        return html.unescape(text.strip()).replace("\xa0", " ")
+
+    name = clean_text(activity.select_one(".title").text)
+    description = clean_text(activity.select_one(".b-typo").text)
+    date = clean_text(activity.select_one(".date").text)
+    price = clean_text(activity.select_one(".when-and-where span:nth-of-type(2)").text)
+
+    return {
+        "name": name,
+        "description": description,
+        "date": date,
+        "price": price
+    }
+
+
+def parse_dou_for_activities():
+    soup = fetch_html(DOU_LVIV_ACTIVITIES)
+    activities = soup.select(".b-postcard")
+    activities_list = [parse_one_dou_activity(activity) for activity in activities]
+    activities_to_send = ""
+    counter = 0
+    for activity in activities_list:
+        counter += 1
+        activities_to_send += f"<b>{counter}.</b> <b>{activity["name"]}</b>.\n<b>Date:</b> {activity["date"]}\n{activity["description"]}\n<b>Price:</b> {activity["price"]}\n\n"
+    activities_to_send += "<a href='https://dou.ua/calendar/city/Lviv/'><b>DOU website</b></a>"
+    return activities_to_send
